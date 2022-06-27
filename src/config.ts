27481ddd729b2f039aa5fd8ba56e369acffa64bd
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2022-06-27 17:25:32 -0300

    openPosition

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import "dotenv/config";
 
 export default {
-  account: {
-    privateKey: process.env.PRIVATE_KEY!,
-  },
   rpc: {
     endpoint: "https://api.mainnet-beta.solana.com",
+  },
+  strategy: {
+    spaces: 10,
   },
 };
--- src/v1.ts
+++ src/v1.ts
@@ -1,72 +1,30 @@
+import "dotenv/config";
 import { Provider } from "@project-serum/anchor";
 import { u64 } from "@solana/spl-token";
 import Decimal from "decimal.js";
-import * as bs58 from "bs58";
-
-import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import { PublicKey } from "@solana/web3.js";
 import {
+  getNearestValidTickIndex,
+  getPrevValidTickIndex,
   OrcaNetwork,
   OrcaWhirlpoolClient,
   PoolData,
   priceToTickIndex,
+  tickIndexToPrice,
 } from "@orca-so/whirlpool-sdk";
-import { getOrca, Orca, OrcaPoolConfig } from "@orca-so/sdk";
 import { solToken, usdcToken } from "@orca-so/sdk/dist/constants/tokens";
 import config from "./config";
 
-function owner(): Keypair {
-  return Keypair.fromSecretKey(bs58.decode(config.account.privateKey));
-}
-
-async function connect(): Promise<Orca> {
-  console.log("Connecting to RPC endpoint...");
-  const connection = new Connection(config.rpc.endpoint, "singleGossip");
-  const orca = getOrca(connection);
-  return orca;
-}
-
-async function swap(
-  orca: Orca,
-  from: "USDC" | "SOL",
-  to: "USDC" | "SOL",
-  amount: number,
-  slippage?: number
-) {
-  console.log("Swapping");
-  const poolConfig = OrcaPoolConfig.SOL_USDC;
-  const pool = orca.getPool(poolConfig);
-  const inputToken = from === "USDC" ? pool.getTokenB() : pool.getTokenA();
-  const inputAmount = new Decimal(amount);
-  const quote = await pool.getQuote(
-    inputToken,
-    inputAmount,
-    slippage ? new Decimal(slippage) : undefined
-  );
-  const outputAmount = quote.getMinOutputAmount();
-
-  console.log(
-    `Swap ${inputAmount.toString()} ${from} for ${outputAmount.toNumber()} ${to} (slippage ${
-      slippage ? `${slippage * 100}%` : "default"
-    })`
-  );
-  const swapPayload = await pool.swap(
-    owner(),
-    inputToken,
-    inputAmount,
-    outputAmount
-  );
-  const swapTxId = await swapPayload.execute();
-  console.log("Swapped:", swapTxId, "\n");
+function tickSpacing(): number {
+  return 64;
 }
 
 function getPoolAddress(whirlpool: OrcaWhirlpoolClient): PublicKey {
-  const tickSpacing = 64;
   const poolAddress = whirlpool.pool.derivePDA(
     solToken.mint,
     usdcToken.mint,
-    tickSpacing
+    tickSpacing()
   ).publicKey;
-  console.log(poolAddress);
   return poolAddress;
 }
 
@@ -81,40 +39,38 @@ async function getPoolData(
   return poolData;
 }
 
-async function main() {
-  // const orca = await connect();
-  // await swap(orca, "SOL", "USDC", 0.25);
-  // await swap(orca, "USDC", "SOL", 10, 0.5 / 100);
+async function openPosition(
+  whirlpool: OrcaWhirlpoolClient,
+  poolAddress: PublicKey,
+  poolData: PoolData,
+  amount: number
+): Promise<void> {
+  const tick = getNearestValidTickIndex(
+    poolData!.price,
+    solToken.scale,
+    usdcToken.scale,
+    tickSpacing()
+  );
+  const tickStart = tick - (tickSpacing() * config.strategy.spaces) / 2;
+  const tickEnd = tick + (tickSpacing() * config.strategy.spaces) / 2;
+  console.log(tick, tickStart, tickEnd);
 
-  const whirlpool = new OrcaWhirlpoolClient({ network: OrcaNetwork.MAINNET });
-
-  const poolAddress = getPoolAddress(whirlpool);
-  const poolData = await getPoolData(whirlpool, poolAddress);
-  console.log(poolData?.liquidity.toString());
-  console.log(poolData?.price.toString());
-  console.log(poolData?.tokenVaultAmountA.toString());
-  console.log(poolData?.tokenVaultAmountB.toString());
-
-  const amount = 1;
+  const priceStart = tickIndexToPrice(
+    tickStart,
+    solToken.scale,
+    usdcToken.scale
+  );
+  const priceEnd = tickIndexToPrice(tickEnd, solToken.scale, usdcToken.scale);
+  console.log(priceStart, priceEnd);
 
   const provider = Provider.env();
-
-  // Open a position
   const openPositionQuote = await whirlpool.pool.getOpenPositionQuote({
     poolAddress,
     tokenMint: solToken.mint,
-    tokenAmount: new u64(amount ** solToken.scale),
+    tokenAmount: new u64(amount * 10 ** solToken.scale),
     refresh: true,
-    tickLowerIndex: priceToTickIndex(
-      new Decimal(0),
-      solToken.scale,
-      usdcToken.scale
-    ),
-    tickUpperIndex: priceToTickIndex(
-      new Decimal(100),
-      solToken.scale,
-      usdcToken.scale
-    ),
+    tickLowerIndex: tickStart,
+    tickUpperIndex: tickEnd,
   });
   const openPositionTx = await whirlpool.pool.getOpenPositionTx({
     provider,
@@ -122,6 +78,25 @@ async function main() {
   });
   const openPositionTxId = await openPositionTx.tx.buildAndExecute();
   console.log(openPositionTxId);
+}
+
+async function main() {
+  const whirlpool = new OrcaWhirlpoolClient({ network: OrcaNetwork.MAINNET });
+
+  const poolAddress = getPoolAddress(whirlpool);
+  const poolData = await getPoolData(whirlpool, poolAddress);
+  if (!poolData) {
+    throw new Error(`Invalid pool address ${poolAddress}`);
+  }
+
+  console.log(poolData.liquidity.toString());
+  console.log(poolData.price.toString());
+  console.log(poolData.tokenVaultAmountA.toString());
+  console.log(poolData.tokenVaultAmountB.toString());
+
+  const amount = 0.1;
+
+  openPosition(whirlpool, poolAddress, poolData, amount);
 }
 
 main();
